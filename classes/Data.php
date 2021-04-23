5ecd81ee5cd2de23b7429e561906f578599f1fbd
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-04-23 15:33:44 +0100

    create_data.php added

--- classes/Data.php
+++ classes/Data.php
@@ -2,6 +2,12 @@
 
 Class Content{
     private $conn ="";
+   public $food_name ='';
+   public $food_introduction = ""; 
+    public $food_procedure ='';
+    public $food_ingredient ='';
+    public $food_image ='';
+    public $food_equipment ='';
 
     public function __construct($conn){
         $this->conn= $conn;
@@ -34,11 +40,33 @@ Class Content{
                  }else{
                      return array('message'=> 'No result');
                  }
-           
-
          }
+
+         public function post_data(){
+            //statement
+            $sql = "INSERT INTO naija_food_prep VALUES(?, ?, ?,?,?,?)";
+              //prepare statement
+              $stmt = $this->conn->prepare($sql);
+              //bind parameter to query
+              $stmt->bindParam(1, $this->food_name);
+              $stmt->bindParam(2, $this->food_introduction);
+              $stmt->bindParam(3, $this->food_procedure);
+              $stmt->bindParam(4, $this->food_ingredient);
+              $stmt->bindParam(5, $this->food_image);
+              $stmt->bindParam(6, $this->food_equipment);
+               // execute query
+                if($stmt->execute()){
+                    return true;
+                }else{
+                    return false;
+                }
+            
+               
+                
+            }
+         
     }
     
     
-
+// $food_name, $food_introduction, $food_procedure, $food_ingredient, $food_image, $food_equipment
 
---
+++ view/create_data.php
@@ -0,0 +1,43 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+ header('Content-Type: application/json');
+ header('Access-Control-Allow-Method: POST');
+
+include '../classes/Data.php';
+include '../database_config/database.php';
+
+$post_obj = new Content($conn);
+
+// get posted data
+$create_data = json_decode(file_get_contents("php://input"));
+if (!empty($create_data->food_name && $create_data->food_introduction && $create_data->food_procedure && $create_data->food_ingredient && $create_data->food_image && $create_data->food_equipment )) {
+   $post_obj->food_name = $create_data->food_name;
+   $post_obj->food_introduction = $create_data->food_introduction;
+   $post_obj->food_procedure = $create_data->food_procedure;
+   $post_obj->food_ingredient = $create_data->food_ingredient;
+   $post_obj->food_image = $create_data->food_image;
+   $post_obj->food_equipment = $create_data->food_equipment;
+
+   if($post_obj->post_data()){
+      // set response code - 201 created
+      http_response_code(201);
+  
+      // tell the user
+      echo json_encode(array("message" => "data was created."));
+   }else{
+       // set response code - 503 service unavailable
+       http_response_code(503);
+  
+       // tell the user
+       echo json_encode(array("message" => "Unable to create data."));
+   }
+}else{
+    // set response code - 400 bad request
+    http_response_code(400);
+  
+    // tell the user
+    echo json_encode(array("message" => "Unable to create data. Data is incomplete."));
+}
+
+
